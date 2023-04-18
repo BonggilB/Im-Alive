8c255d32af4a4a21376470bed18d97cb3e9464ed
BonggilB <95198463+BonggilB@users.noreply.github.com>
2023-04-18 13:52:07 +0900

    Add files via upload

---
+++ TCP도구/Network/NetListener.cs
@@ -0,0 +1,171 @@
+﻿using Action;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace MizeGSDK.Network
+{
+    public class NetListener
+    {
+        #region 변수
+        private TcpListener listener;
+
+        private List<NetServerClient> clients = new List<NetServerClient> ();
+
+        public Action<NetServerClient, byte[], int> onReceiveData;
+
+        public Action<NetServerClient> onClientDisconnect;
+
+        private object closeObj = new object ();
+        #endregion
+
+
+        #region Methods
+        /// <summary>
+        /// Start
+        /// </summary>
+        public void start (int port, Action<NetServerClient, byte[], int> onDataReceived, Action<NetServerClient> onClientDisconnect)
+        {
+            this.onReceiveData = onDataReceived;
+
+            this.onClientDisconnect = onClientDisconnect;
+            // 처음 start 하는데 문제가 생길시 아래stop() 주석
+            stop ();
+
+            IPEndPoint endPoint = new IPEndPoint (IPAddress.Any, port);
+
+
+            listener = new TcpListener (endPoint);
+
+            startListening ();
+        }
+
+
+        /// <summary>
+        /// Start Listening
+        /// </summary>
+        private void
+        startListening ()
+        {
+            System.Action action = () =>
+            {
+                try
+                {
+                    listener?.Start ();
+
+                    while (listener != null)
+                    {
+                        TcpClient client = null;
+
+                        try
+                        {
+                            client = listener?.AcceptTcpClient ();
+                        }
+                        catch (Exception)
+                        {
+                            break;
+                        }
+
+                        NetServerClient netClient = new NetServerClient (client,
+                                                                         onClientDataReceived,
+                                                                         onClientDisconnected);
+
+                        clients.Add (netClient);
+                    }
+                }
+                finally
+                {
+                    stop ();
+                }
+            };
+
+
+            Thread thread = new Thread (() => action.Invoke ());
+
+            thread.Start ();
+        }
+
+
+        /// <summary>
+        /// On Client disconnected
+        /// </summary>
+        /// <param name="sender"></param>
+        private void
+        onClientDisconnected (NetServerClient sender)
+        {
+            onClientDisconnect?.Invoke (sender);
+
+            clients.RemoveAll (x => x == sender);
+        }
+
+
+        /// <summary>
+        /// Data recieved
+        /// </summary>
+        private void
+        onClientDataReceived (NetServerClient sender,
+                              byte[] data,
+                              int len)
+        {
+            onReceiveData?.Invoke (sender,
+                                   data,
+                                   len);
+
+            parse(sender,
+                                        data,
+                                        len);
+        }
+
+
+        /// <summary>
+        /// Stop
+        /// </summary>
+        public void
+        stop ()
+        {
+            lock (closeObj)
+            {
+                if (null == listener)
+                {
+                    return;
+                }
+
+
+                disconnectAllClients ();
+
+
+                listener?.Server?.Close ();
+
+                listener?.Stop ();
+
+                listener = null;
+            }
+        }
+
+
+        /// <summary>
+        /// Disconnect all clients
+        /// </summary>
+        private void
+        disconnectAllClients ()
+        {
+            if (null != clients)
+            {
+                clients.ForEach (x => x.disconnect ());
+
+                clients.Clear ();
+            }
+        }
+        #endregion
+
+        public static void
+        parse(NetServerClient client,
+               byte[] data,
+               int len)
+        {
+            /// Parse the command
+        }
+    }
+}
---
+++ TCP도구/Network/NetServerClient.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Net.Sockets;
+
+
+namespace MizeGSDK.Network
+{
+    public class NetServerClient
+    {
+        #region 변수
+        private TcpClient client;
+
+        private Action<NetServerClient, byte[], int> dataReceiveCallback = null;
+
+        private Action<NetServerClient> clientDisconnectedCallback = null;
+
+        private const int C_BUFFER_SIZE = 131072;
+        #endregion
+
+
+        #region Methods
+        /// <summary>
+        /// Ctr
+        /// </summary>
+        /// <param name="client"></param>
+        public
+        NetServerClient(TcpClient client,
+                         Action<NetServerClient, byte[], int> dataReceiveCallback,
+                         Action<NetServerClient> clientDisconnectedCallback)
+        {
+            this.client = client;
+
+            this.dataReceiveCallback = dataReceiveCallback;
+
+            this.clientDisconnectedCallback = clientDisconnectedCallback;
+
+
+            startListening();
+        }
+
+
+        /// <summary>
+        /// Start Listening to server
+        /// </summary>
+        private void startListening()
+        {
+            //try
+            //{
+            while ((client != null) && client.Connected && client.Available != 0 )
+            {
+                byte[] data = new byte[C_BUFFER_SIZE];
+                var ㅋㅋ = client.Available;
+                
+                if (null == client?.GetStream())
+                {
+                    break;
+                }
+                try
+                {
+                    int len = client.GetStream()
+                                    .Read(data,
+                                           0,
+                                           data.Length);
+
+                    if (len == 0)
+                    {
+                        break;
+                    }
+
+                    byte[] pureData = new byte[len];
+
+                    Array.Copy(data,
+                                pureData,
+                                len);
+
+                    dataReceiveCallback?.Invoke(this,
+                                                 pureData,
+                                                 len);
+                }
+                catch
+                {
+                    //ㅁㄴㅇㄹ
+                }
+            }
+            //}
+            //finally
+            //{
+            //    disconnect();
+            //    //client.Dispose();
+            //    //client = new TcpClient ();
+            //}
+
+            MizeGSDK.listenerReload();
+        }
+
+
+        /// <summary>
+        /// Write byte
+        /// </summary>
+        public void
+        write(string data)
+        {
+            byte[] bData = System.Text.Encoding.UTF8.GetBytes(data);
+
+            write(bData);
+        }
+
+
+        /// <summary>
+        /// Write byte
+        /// </summary>
+        /// <param name="data"></param>
+        public void
+        write(byte[] data)
+        {
+            try
+            {
+                client?.GetStream()
+                       .Write(data,
+                               0,
+                               data.Length);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+
+
+        /// <summary>
+        /// Disconnect
+        /// </summary>
+        public void
+        disconnect()
+        {
+            //write("DISCONNECT");
+
+            if (client != null)
+            {
+                try
+                {
+                    client.GetStream()?.Close();
+                }
+                catch
+                {
+                    client.Dispose();
+                }
+                client?.Close();
+            }
+
+            clientDisconnectedCallback?.Invoke(this);
+        }
+        #endregion
+    }
+}
